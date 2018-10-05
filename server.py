96827d4edea623179692f1e4cfdc11d107ff95c3
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-10-05 16:52:45 +0300

    Added favicon

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def form():
-    return render_template('form.html')
+    return render_template(
+        'form.html',
+        favicon_url=url_for('static', filename='favicon.ico'),
+    )
+
 
 if __name__ == "__main__":
     app.run()
