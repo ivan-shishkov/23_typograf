a7e3824bd564a829fbbde63d8a2567d9df8457d6
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-10-06 15:10:48 +0300

    Added template variables to render both source and processed text

--- server.py
+++ server.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
-    return render_template('form.html')
+    source_text = formatted_text = ''
+    if request.method == 'POST':
+        source_text = request.form['text']
+        formatted_text = request.form['text']
+    return render_template(
+        'form.html',
+        source_text=source_text,
+        formatted_text=formatted_text,
+    )
 
 
 if __name__ == "__main__":
